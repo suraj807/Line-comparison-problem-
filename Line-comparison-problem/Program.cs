2785a6399a04221a53675a10084931e303ad5cb6
suraj807 <surajdev805@gmail.com>
2022-05-01 13:25:19 +0530

    calculate a length

--- Line-comparison-problem/Program.cs
+++ Line-comparison-problem/Program.cs
@@ -11,6 +11,33 @@ namespace Line_comparison_problem
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to line comparison problem");
+
+            double X1;
+            double Y1;
+            double X2;
+            double Y2;
+            double Length;
+
+
+            Console.WriteLine("Enter first point X coordinate x1: ");
+            X1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first point Y coordinate y1: ");
+            Y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point X coordinate x2: ");
+            X2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point Y coordinate y2: ");
+            Y2 = Convert.ToDouble(Console.ReadLine());
+
+            double Xpower = Math.Pow((X2 - X1), 2);
+            double Ypower = Math.Pow((Y2 - Y1), 2);
+
+            Length = Math.Sqrt((Xpower + Ypower));
+
+            Console.WriteLine($"Length of line using two points ({X1},{X1}) and ({Y2},{Y2}) is : {Length}");
+
         }
     }
 }
