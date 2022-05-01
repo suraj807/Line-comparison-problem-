4735b918a1b4b1b2819f1d86b816af3143f3fadc
suraj807 <surajdev805@gmail.com>
2022-05-01 13:34:30 +0530

    equals of two line

--- Line-comparison-problem/Program.cs
+++ Line-comparison-problem/Program.cs
@@ -12,11 +12,11 @@ namespace Line_comparison_problem
         {
             Console.WriteLine("welcome to line comparison problem");
 
-            double X1;
-            double Y1;
-            double X2;
-            double Y2;
-            double Length;
+            double X1, X2;
+            double Y1, Y2;
+            double x1, x2;
+            double y1, y2;
+            double Lengthline1, Lengthline2;
 
 
             Console.WriteLine("Enter first point X coordinate x1: ");
@@ -34,10 +34,35 @@ namespace Line_comparison_problem
             double Xpower = Math.Pow((X2 - X1), 2);
             double Ypower = Math.Pow((Y2 - Y1), 2);
 
-            Length = Math.Sqrt((Xpower + Ypower));
+            Lengthline1 = Math.Sqrt((Xpower + Ypower));
 
-            Console.WriteLine($"Length of line using two points ({X1},{X1}) and ({Y2},{Y2}) is : {Length}");
+            Console.WriteLine($"Length of line using two points ({X1},{X1}) and ({Y2},{Y2}) is : {Lengthline1}");
 
+
+
+
+            Console.WriteLine("Enter first point X coordinate x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first point Y coordinate y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point X coordinate x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point Y coordinate y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double xpower = Math.Pow((x2 - x1), 2);
+            double ypower = Math.Pow((y2 - y1), 2);
+
+            Lengthline2 = Math.Sqrt((xpower + ypower));
+
+
+            Console.WriteLine($"Length of line using two points ({x1},{x1}) and ({y2},{y2}) is : {Lengthline2}");
+
+            Console.Write("\n\nFirst Line is Equal To Second Line : ");
+            Console.WriteLine((Lengthline1).Equals(Lengthline2));
         }
     }
 }
